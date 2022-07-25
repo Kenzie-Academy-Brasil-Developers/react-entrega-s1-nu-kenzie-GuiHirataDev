734310a25672160a32431b24e6beb3e55d82a541
unknown <guilhermekhirata@hotmail.com>
2022-07-24 22:47:45 -0300

    feat/ adicionando funcionalides finais

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,24 @@
-import Form from './components/form';
-import List from './components/list';
+import MainContent from './components/mainContent';
 import { useState } from 'react';
 import './App.css';
 
 
 function App() {
 
-  const [listTransactions,setListTransactions] = useState([])
+  const [init, setInit] = useState(false)
 
-  const addList = (newItemList) => {
-    setListTransactions([...listTransactions, newItemList])
+  const SetInit = () => {
+    setInit(true)
   }
 
-  const totalValue = listTransactions.reduce((accumulator, actualValue) => {
-    return parseInt(actualValue.value) + accumulator
-  }, 0)
-
-  const handleList = (item) => {
-    const listFiltered = listTransactions.filter((elem) => (elem !== item))
-    setListTransactions(listFiltered)
+  const SetInitFalse = () => {
+    setInit(false)
   }
 
   return (
-    <div className='divPrincipal'>
-      <header>
-          <h1><span>Nu</span> Kenzie</h1>
-          <button>Inicio</button>
-      </header>
-      <main className='container'>
-        <section className='form'>
-          <Form addList={addList}/>
-          <div className='divTotalValue'>
-            <h3>Valor total: <span className='totalValue'>$ {totalValue}</span></h3>
-            <p>O valor se refere ao saldo</p>
-          </div>
-        </section>
-        <section className='cards'>
-          <div className='divCards'>
-            <h3>Resumo financeiro</h3>
-            <div className='divButtons'>
-            <button>Todos</button>
-            <button>Entradas</button>
-            <button>Despesas</button>
-            </div>
-          </div>
-          <List listTransactions= {listTransactions} handleList={handleList}/>
-        </section>
-      </main>
-    </div>
+    <>
+      <MainContent init={init} SetInit={SetInit} SetInitFalse={SetInitFalse}/>
+    </>
   );
 }
 
---
+++ src/components/mainContent/index.jsx
@@ -0,0 +1,84 @@
+import Form from '../form';
+import List from '../list';
+import { useState } from 'react';
+import Image from '../imgs/image.png'
+
+const MainContent = ({init, SetInit, SetInitFalse}) => {
+
+    const [listTransactions,setListTransactions] = useState([])
+
+    const addList = (newItemList) => {
+    setListTransactions([...listTransactions, newItemList])
+    }
+
+    const totalValue = listTransactions.reduce((accumulator, actualValue) => {
+    if(actualValue.type === "Entrada"){
+      return parseInt(actualValue.value) + accumulator
+    } else if(actualValue.type === "Despesa"){
+      return accumulator - actualValue.value
+    }
+    
+    }, 0)
+
+    const handleList = (item) => {
+    const listFiltered = listTransactions.filter((elem) => (elem !== item))
+    setListTransactions(listFiltered)
+    }
+
+    //Filtro não funciona muito bem, pois está setando uma nova lista e perdendo a antiga, mas como era uma parte extra do projeto, não dei muito foco para esta parte
+
+    const entryFilter = () => {
+    const listEntry = listTransactions.filter((elem) => (elem.type === "Entrada"))
+    setListTransactions(listEntry)
+    }
+
+    const expensesFilter = () => {
+    const listExpenses = listTransactions.filter((elem) => (elem.type === "Despesa"))
+    setListTransactions(listExpenses)
+    }
+
+    return (
+        init ? 
+        <div className='divPrincipal'>
+        <header>
+            <h1><span>Nu</span> Kenzie</h1>
+            <button onClick={() => SetInitFalse()}>Inicio</button>
+        </header>
+        <main className='container'>
+        <section className='form'>
+            <Form addList={addList}/>
+            <div className='divTotalValue'>
+                <h3>Valor total: <span className='totalValue'>$ {totalValue}</span></h3>
+                <p>O valor se refere ao saldo</p>
+            </div>
+            </section>
+            <section className='cards'>
+            <div className='divCards'>
+                <h3>Resumo financeiro</h3>
+                <div className='divButtons'>
+                <button>Todos</button>
+                <button onClick={() => entryFilter()}>Entradas</button>
+                <button onClick={() => expensesFilter()}>Despesas</button>
+                </div>
+            </div>
+            <List listTransactions= {listTransactions} handleList={handleList}/>
+            </section>
+        </main>
+        </div>
+        :
+        <main className='mainInit'>
+            <div className='initContent'>
+                <h2><span>Nu</span> Kenzie</h2>
+                <p className='pContent'>Centralize o controle das suas finanças</p>
+                <p className='pDescription'>de forma rápida e segura.</p>
+                <button onClick={() => SetInit()}>Iniciar</button>
+            </div>
+            <div className='initImg'>
+                <img src={Image}/>
+            </div>
+        </main>
+        
+    )
+}
+
+export default MainContent
